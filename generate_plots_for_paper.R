1fd42c27fffe90143bfcd53d7ea69a355001c2d6
jrodu <jordan.rodu@gmail.com>
2026-03-18 15:22:12 -0400

    generate plots for paper

---
+++ generate_plots_for_paper.R
@@ -0,0 +1,15 @@
+w=7
+h=3.65
+
+lik_ratio_plot(59, c(53, 65), .01, c(.004, .02))
+ggsave("LR_glass.pdf", width=w, height = h, units="in")
+lik_ratio_plot(7.255, c(7.05, 7.45), .01, c(.004, .02))
+ggsave("LR_glass_t.pdf", width=w, height = h, units="in")
+lik_ratio_plot(10, c(8, 12), .8, c(.6, 1))
+ggsave("LR_10_dot8.pdf", width=w, height = h, units="in")
+lik_ratio_plot(10, c(8, 12), .2, c(.05, .35))
+ggsave("LR_10_dot2.pdf", width=w, height = h, units="in")
+lik_ratio_plot(2, c(1, 3), .8, c(.6, 1))
+ggsave("LR_2_dot8.pdf", width=w, height = h, units="in")
+lik_ratio_plot(2, c(1, 3), .2, c(.05, .35))
+ggsave("LR_2_dot2.pdf", width=w, height = h, units="in")
