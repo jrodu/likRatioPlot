064f75126e19e69a3e72e1bb1d9f79e8c64f0239
jrodu <jordan.rodu@gmail.com>
2026-04-17 15:48:18 -0400

    fix figures for paper

--- generate_plots_for_paper.R
+++ generate_plots_for_paper.R
@@ -1,6 +1,7 @@
 w=7
 h=3.65
 
+devtools::load_all()
 library(ggplot2) # for ggsave
 
 lik_ratio_plot(59, c(53, 65), .01, c(.004, .02))
@@ -15,7 +16,13 @@ lik_ratio_plot(2, c(1, 3), .8, c(.6, 1))
 ggsave("LR_2_dot8.pdf", width=w, height = h, units="in")
 lik_ratio_plot(2, c(1, 3), .2, c(.05, .35))
 ggsave("LR_2_dot2.pdf", width=w, height = h, units="in")
-lik_ratio_plot(13, c(.65, 25.35), 1.085, c(.67, 1.5))
+lik_ratio_plot(13, c(11, 18), 1, c(.67, 1.5))
 ggsave("LR_L_hpo.pdf", width=w, height = h, units="in")
-lik_ratio_plot(13, c(.65, 25.35), .27, c(.11, .43))
-ggsave("LR_L_LPO.pdf", width=w, height = h, units="in")
+lik_ratio_plot(13, c(11, 18), .25, c(.11, .43)) +
+  scale_y_continuous(breaks=c(1, 1.21, 3.25, 7.74),
+                     labels = c("\n1", "1.21", "3.25", "7.74")) #add jitter
+                                                        #to '1' on y axis to
+                                                        #avoid clash with 1.21.
+                                                        #Clashes when using aspect ratio
+                                                        #for publication plot
+ggsave("LR_L_lpo.pdf", width=w, height = h, units="in")
